435972cf9470eec023d9416243ad2a4f318200ec
amir vansover <amir@vansover.com>
2020-07-09 00:37:27 +0300

    Updates

---
+++ precache-manifest.2404903e42b0e2fb1b365c8d4ec98c17.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "9859571fa0ce26304a64c28438598694",
+    "url": "/weeklyplanner2/index.html"
+  },
+  {
+    "revision": "74db082f28825ff28255",
+    "url": "/weeklyplanner2/static/css/main.a43544ee.chunk.css"
+  },
+  {
+    "revision": "49cb69c2e914193257d8",
+    "url": "/weeklyplanner2/static/js/2.88affc85.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/weeklyplanner2/static/js/2.88affc85.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "74db082f28825ff28255",
+    "url": "/weeklyplanner2/static/js/main.fc5894cc.chunk.js"
+  },
+  {
+    "revision": "25b5bdc3160ddaa18edd",
+    "url": "/weeklyplanner2/static/js/runtime-main.80a79e7d.js"
+  }
+]);
