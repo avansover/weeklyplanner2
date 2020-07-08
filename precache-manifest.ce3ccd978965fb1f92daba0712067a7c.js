4fa5b24e284c04b5b3157a6eec046a678751bc3c
amir vansover <amir@vansover.com>
2020-07-08 23:38:32 +0300

    Updates

---
+++ precache-manifest.ce3ccd978965fb1f92daba0712067a7c.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "e17ad6022ab8d077d63659040c6ea85d",
+    "url": "/index.html"
+  },
+  {
+    "revision": "3df8b65cca8c9f8e7eba",
+    "url": "/static/css/main.a43544ee.chunk.css"
+  },
+  {
+    "revision": "49cb69c2e914193257d8",
+    "url": "/static/js/2.88affc85.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/static/js/2.88affc85.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "3df8b65cca8c9f8e7eba",
+    "url": "/static/js/main.0c3a9f21.chunk.js"
+  },
+  {
+    "revision": "62299d50b145504a046a",
+    "url": "/static/js/runtime-main.2ba6898c.js"
+  }
+]);
