8e45c22e13679e5723fd4a2de25a0d61a6a10936
amir vansover <amir@vansover.com>
2020-07-31 19:36:13 +0300

    Updates

--- precache-manifest.5ddfe046a4465ce26bae37c12ffd22c1.js
+++ precache-manifest.5ddfe046a4465ce26bae37c12ffd22c1.js
@@ -1,27 +1,27 @@
 self.__precacheManifest = (self.__precacheManifest || []).concat([
   {
-    "revision": "63fd3be8095464fd8ac3b8cb5662e9fd",
+    "revision": "62fa0dcb4d04394d1ab0907b01bd9ea9",
     "url": "/weeklyplanner2/index.html"
   },
   {
-    "revision": "9d9ae13c243962660228",
+    "revision": "c1bd13b777590beba930",
     "url": "/weeklyplanner2/static/css/2.af3c1da9.chunk.css"
   },
   {
-    "revision": "1caf36ee7293e1e066ff",
+    "revision": "48630b63af7bd9d79ae7",
     "url": "/weeklyplanner2/static/css/main.4a34a1ec.chunk.css"
   },
   {
-    "revision": "9d9ae13c243962660228",
-    "url": "/weeklyplanner2/static/js/2.1b7d6c16.chunk.js"
+    "revision": "c1bd13b777590beba930",
+    "url": "/weeklyplanner2/static/js/2.71a3838e.chunk.js"
   },
   {
     "revision": "3453b8997016469371284a28c0e873e2",
-    "url": "/weeklyplanner2/static/js/2.1b7d6c16.chunk.js.LICENSE.txt"
+    "url": "/weeklyplanner2/static/js/2.71a3838e.chunk.js.LICENSE.txt"
   },
   {
-    "revision": "1caf36ee7293e1e066ff",
-    "url": "/weeklyplanner2/static/js/main.f5cc8bd3.chunk.js"
+    "revision": "48630b63af7bd9d79ae7",
+    "url": "/weeklyplanner2/static/js/main.1307d56c.chunk.js"
   },
   {
     "revision": "25b5bdc3160ddaa18edd",
